c240aff72a708961968b5263d3bf5e0590d2f41c
Alessia Lorusso <alessial@aurigaactive.aurigainformatica.it>
2022-03-23 17:06:39 +0100

    Prima visualizzazione della list con AntD

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.png';
 import './App.scss';
-import List from './List/index.tsx'
+import CharactersList from './List/index.tsx'
 import React, { useState } from 'react';
 
 const App = ()=> {
@@ -10,12 +10,12 @@ const [showList, setShowList] = useState(false);
   return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={()=>setShowList(!showList)}>
-          Lista dei Personaggi
-        </button>
-        {showList && <List title={'Ecco la lista'}/>}
+        <img src={'logo'} className="App-logo" alt="logo" />
       </header>
+      <button onClick={()=>setShowList(!showList)}>
+        Lista dei Personaggi
+      </button>
+      {showList && <CharactersList title={'Ecco la lista'} characters={[{name: 'Nome 1'},{name: 'Nome 2'}]} />}
     </div>
   );
 }
--- src/List/index.tsx
+++ src/List/index.tsx
@@ -1,13 +1,35 @@
-import React, { Component } from 'react';
+import { List, Modal } from 'antd';
+import React, { Component, useState } from 'react';
+import './index.scss';
 
 interface Props {
     title: string,
-    characters: Array<any>
-}
-export class List extends Component<Props> {
-    render(): JSX.Element {
-        return <div>{this.props.title}</div>
-    }
+    characters: Array<{name:string}>
 }
 
-export default List;
+const CharactersList: React.FC<Props> = props => {
+
+    const [showDetail, setShowDetail] = useState(false);
+    
+        return (<>
+            <List 
+                size="small"
+                //bordered={true}
+                className='characters-list'
+                dataSource={props.characters}
+                renderItem={(character) => {
+                    return <List.Item onClick={()=>setShowDetail(!showDetail)}>{character.name}</List.Item>
+                }}
+                /*itemLayout="vertical"
+                pagination={{
+                    onChange: page => {
+                      console.log(page);
+                    },
+                    pageSize: 1,
+                  }}*/
+            />
+            <Modal visible={showDetail}>Ciao</Modal>
+        </>)
+}
+
+export default CharactersList;
